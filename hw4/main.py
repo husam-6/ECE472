631cd9ad31eea8b01ab7273a0524eaae1eaf4d16
husam-6 <husam@almanakly.com>
2022-09-25 21:27:02 -0400

    Add data class for CIFAR10 / 100 datasets

--- hw4/main.py
+++ hw4/main.py
@@ -3,6 +3,7 @@ Homework assignment 4 - Husam Almanakly
 
 """
 
+from ctypes.wintypes import HACCEL
 import os
 import logging
 import matplotlib
@@ -14,6 +15,7 @@ from tensorflow.keras import layers, models
 import argparse
 from tqdm import trange
 from dataclasses import dataclass, field, InitVar
+from typing import Tuple
 import pickle
 
 script_path = os.path.dirname(os.path.realpath(__file__))
@@ -23,8 +25,122 @@ matplotlib.style.use("classic")
 # Command line flags
 parser = argparse.ArgumentParser()
 parser.add_argument("--random_seed", default=31415, help="Random seed")
-parser.add_argument("--epochs", default=31415, help="Number of Epochs")
+parser.add_argument("--epochs", default=5, help="Number of Epochs")
 parser.add_argument("--debug", default=False, help="Set logging level to debug")
+
+# Image dimensions
+H = 32
+W = 32
+C = 3
+
+
+@dataclass
+class Data:
+    """Data Class for CIFAR Data. Obtained from: 
+    
+    https://www.cs.toronto.edu/~kriz/cifar.html
+    """
+   
+    # Training data
+    cf10_train: np.ndarray = field(init=False)
+    cf10_train_labels: np.ndarray = field(init=False)
+    
+    cf100_train: np.ndarray = field(init=False)
+    cf100_train_labels: np.ndarray = field(init=False)
+    
+    # Validation Set
+    cf10_val: np.ndarray = field(init=False)
+    cf10_val_labels: np.ndarray = field(init=False)
+    
+    cf100_val: np.ndarray = field(init=False)
+    cf100_val_labels: np.ndarray = field(init=False)
+
+    # Test data
+    cf10_test: np.ndarray = field(init=False)
+    cf10_test_labels: np.ndarray = field(init=False)
+    
+    cf100_test: np.ndarray = field(init=False)
+    cf100_test_labels: np.ndarray = field(init=False)
+
+    cf10_label_names: list = field(init=False)
+    cf100_label_names: list = field(init=False)
+
+
+    def __post_init__(self):
+        """Data should be in N x H x W x C format 
+        
+        N = num samples
+        H x W is the dimensions of the image
+        C is the color channel (ie RGB => C = 3)
+        """
+
+        # Read in pickled data
+        cf10, cf10_labels = unpack_cf10_data()
+        cf100 = unpickle(f"{script_path}/cifar-100-python/train")
+        cf10_test = unpickle(f"{script_path}/cifar-10-batches-py/test_batch")
+        cf100_test = unpickle(f"{script_path}/cifar-100-python/test")
+        
+        # Label names
+        cf10_label_names = unpickle(f"{script_path}/cifar-10-batches-py/batches.meta")["label_names"]
+        cf100_label_names = unpickle(f"{script_path}/cifar-100-python/meta")["fine_label_names"]
+        
+        self.cf10_label_names = [y.decode("ascii") for y in cf10_label_names]
+        self.cf100_label_names = [y.decode("ascii") for y in cf100_label_names]
+        
+        # CIFAR10 Training Data - use last 10,000 samples as validation set
+        self.cf10_train = cf10[:40000, :, :, :]
+        self.cf10_val = cf10[40000:, :, :, :]
+
+        self.cf10_train_labels = cf10_labels[:40000]
+        self.cf10_val_labels = cf10_labels[40000:]
+
+        self.cf10_test = rgb_stack(cf10_test["data"])
+        self.cf10_test_labels = cf10_test["labels"]
+
+        # CIFAR100 data
+        cf100_data = cf100["data"]
+        self.cf100_train = rgb_stack(cf100_data)[:40000, :, :, :]
+        self.cf100_val = rgb_stack(cf100_data)[40000:, :, :, :]
+        
+        self.cf100_train_labels = cf100['fine_labels'][:40000]
+        self.cf100_val_labels = cf100['fine_labels'][40000:]
+        
+        self.cf100_test = rgb_stack(cf100_test["data"])
+        self.cf100_test_labels = cf100_test["fine_labels"]
+
+
+def unpack_cf10_data() -> Tuple[np.ndarray]:
+    """Combine all 5 'batches' in CIFAR10 data"""
+    labels = []
+    cf10 = []
+    for batch in range(1, 6):
+        unpickled_data = unpickle(f"{script_path}/cifar-10-batches-py/data_batch_{batch}")
+        im = unpickled_data["data"]
+        labels.append(unpickled_data["labels"])
+
+        cf10.append(rgb_stack(im))
+
+    
+    labels = np.concatenate(labels, axis=0)
+    cf10 = np.concatenate(cf10, axis=0)
+
+    return cf10, labels
+
+
+def rgb_stack(im: np.ndarray) -> np.ndarray:
+    """Function to reformat input Numpy array to be H x W x C
+    
+    Input should be an N x 3072 numpy array where N = number of 
+    samples
+    """
+    assert im.shape[1] == 3072
+    assert len(im.shape) == 2
+
+    im_r = im[:, 0:1024].reshape(-1, 32, 32)
+    im_g = im[:, 1024:2048].reshape(-1, 32, 32)
+    im_b = im[:, 2048:].reshape(-1, 32, 32)
+
+    return np.stack([im_r, im_g, im_b], axis=-1)
 
 
 def unpickle(file):
@@ -34,6 +150,7 @@ def unpickle(file):
     """
     with open(file, 'rb') as fo:
         dict = pickle.load(fo, encoding='bytes')
+    
     return {y.decode('ascii'): dict.get(y) for y in dict.keys()}
 
 
@@ -48,7 +165,7 @@ def create_model():
     model = models.Sequential()
 
     # CNN Layers
-    model.add(layers.Conv2D(15, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+    model.add(layers.Conv2D(15, (3, 3), activation='relu', input_shape=(H, W, C)))
     model.add(layers.MaxPooling2D((2, 2)))
     model.add(layers.Conv2D(15, (3, 3), activation='relu'))
     model.add(layers.MaxPooling2D((2, 2)))
@@ -90,30 +207,23 @@ def main():
     if args.debug:
         logging.getLogger().setLevel(logging.DEBUG)
     
+
     # Process data
-    labels = []
-    images = []
-    for batch in range(1, 6):
-        logging.info(f"Unpacking batch {batch}")
-        unpickled_data = unpickle(f"{script_path}/cifar-10-batches-py/data_batch_{batch}")
-        # logging.info(data_test['data'])
-        im = unpickled_data["data"]
-        labels.append(unpickled_data["labels"])
-        
-        im_r = im[:, 0:1024].reshape(-1, 32, 32)
-        im_g = im[:, 1024:2048].reshape(-1, 32, 32)
-        im_b = im[:, 2048:].reshape(-1, 32, 32)
-
-        images.append(np.stack([im_r, im_g, im_b], axis=-1))
-
-    labels = np.concatenate(labels, axis=0)
-    images = np.concatenate(images, axis=0)
-    logging.info(f"Class: {labels[0]}")
-    logging.info(images.shape)
+    logging.info("Reading in pickled data...")
+    data = Data()
 
     # Plot image as example
     plt.figure()
-    plt.imshow(images[0, :, :, :])
+    fig, ax = plt.subplots(1, 2, figsize=(10, 3), dpi=200)
+    label = data.cf10_label_names[data.cf10_train_labels[0]]
+    label2 = data.cf100_label_names[data.cf100_train_labels[0]]
+    
+    ax[0].imshow(data.cf10_train[0, :, :, :])
+    ax[0].set_title(f"CIFAR10: {label}")
+
+    ax[1].imshow(data.cf100_train[0, :, :,:])
+    ax[1].set_title(f"CIFAR100: {label2}")
+
     plt.savefig(f"{script_path}/cifar.pdf")
 
 
