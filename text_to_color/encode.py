e3bf7906a41e666e236ce802f4fa1cbabdf1bf27
husam-6 <husam@almanakly.com>
2022-12-08 22:28:00 -0500

    Add code to save tfexample objects for each input

--- text_to_color/encode.py
+++ text_to_color/encode.py
@@ -10,6 +10,8 @@ import time
 from skimage import io, color
 from skimage.transform import resize
 import matplotlib.pyplot as plt
+import tensorflow as tf
+import tfrecord
 
 
 def main():
@@ -20,14 +22,17 @@ def main():
     logging.basicConfig(level=logging.INFO)
     get_embeddings(model, tok)
 
+
 def get_embeddings(model, tok):
-    # start = time.time()
-    for i in range(1):
-        path_to_file = f"./captions/caption{i}.txt"
+    record = []
+    for i in range(3):
+        # Get paths to image and caption
+        path_to_file = f"./laion/captions/caption{i}.txt"
         path_to_img = f"./laion/images/img{i}.jpeg"
         file_exists = exists(path_to_file)
         rgb_exists = exists(path_to_img)
-        if not file_exists or not gray_exists or not rgb_exists:
+        if not file_exists or not rgb_exists:
+            print("Image doesnt exist")
             continue
         with open(path_to_file) as f:
             lines = f.readlines()
@@ -36,34 +41,35 @@ def get_embeddings(model, tok):
         rgb = resize(io.imread(path_to_img), (256, 256))
         gray = color.rgb2gray(rgb)
         labrgb = color.rgb2lab(rgb)
-        # print(labrgb.shape)
-        # print(gray.shape)
-        
+        assert labrgb.shape == (256, 256, 3)
+        assert gray.shape == (256, 256)
+
         # Pass tokens forward through encoder network
         enc = tok(lines, max_length=1024, truncation=True, return_tensors="pt")
-        
-        plt.figure()
-        fig, ax = plt.subplots(1,2)
-        ax[0].imshow(gray, cmap="gray")
-        ax[1].imshow(rgb, cmap="gray")
-        plt.savefig("./data_test.pdf")
-        # forward pass through encoder only
         output = model.encoder(
             input_ids=enc["input_ids"], 
             attention_mask=enc["attention_mask"], 
             return_dict=True
         )
 
-        # get the final hidden states
+        # Get the final hidden states
         emb = output.last_hidden_state.detach().numpy().squeeze()
         fixed_emb = emb.mean(axis=0)
-        logging.info(f"Creating TFRecord for object: {i}") 
         assert fixed_emb.shape[0] == 1024
-        embeddings.append(fixed_emb)
-    # end = time.time()
-    # print(end - start)
-    
 
+        record.append(tfrecord.image_example(labrgb, gray, fixed_emb))
+        logging.info(f"Creating TFRecord for object: {i}") 
+        
+        # Write to tfrecords file in batches
+        if i == 2:
+            # Write to tfrecords file 
+            record_file = 'images.tfrecords'
+            with tf.io.TFRecordWriter(record_file) as writer:
+                for i in range(3):
+                    tf_example = record[i]
+                    writer.write(tf_example.SerializeToString())
+            # Reset record
+            record = []
 
 
 if __name__ == "__main__":
