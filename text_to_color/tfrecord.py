f1a52249ebead4d556487808fe5f6a3dfd7e5544
husam-6 <husam@almanakly.com>
2022-12-08 22:07:48 -0500

    Reformat file to allow for use in other modules

--- text_to_color/tfrecord.py
+++ text_to_color/tfrecord.py
@@ -13,37 +13,13 @@ from skimage.transform import resize
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
-path_to_file = f"./tmp/caption15.txt"
-path_to_img = f"./tmp/img15.jpeg"
-file_exists = exists(path_to_file)
-rgb_exists = exists(path_to_img)
-if not file_exists or not rgb_exists:
-    print("Image doesnt exist")
-    raise AssertionError
-with open(path_to_file) as f:
-    lines = f.readlines()
+# Create a dictionary describing the features.
+feature_desc = {
+    'image_raw': tf.io.FixedLenFeature([], tf.string),
+    'image_gray': tf.io.FixedLenFeature([], tf.string),
+    'caption_emb': tf.io.FixedLenFeature([], tf.string),
+}
 
-# Get Images
-rgb = resize(io.imread(path_to_img), (256, 256))
-gray = color.rgb2gray(rgb)
-labrgb = color.rgb2lab(rgb)
-
-model = T5Model.from_pretrained("t5-small")
-tok = T5Tokenizer.from_pretrained("t5-small")
-
-enc = tok(lines, max_length=1024, truncation=True, return_tensors="pt")
-
-output = model.encoder(
-            input_ids=enc["input_ids"], 
-            attention_mask=enc["attention_mask"], 
-            return_dict=True)
-
-# get the final hidden states
-emb = output.last_hidden_state.detach().numpy().squeeze()
-fixed_emb = emb.mean(axis=0)
-
-
-# %%
 
 def _bytes_feature(value):
   """Returns a bytes_list from a string / byte."""
@@ -52,8 +28,7 @@ def _bytes_feature(value):
   return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
 
-# %% Write to TFRecords file for an image
-
+# Write to TFRecords file for an image
 # Create a dictionary with features that may be relevant.
 def image_example(image_arr, gray_arr, caption_emb):
     image_string = image_arr.tobytes()
@@ -69,46 +44,75 @@ def image_example(image_arr, gray_arr, caption_emb):
     return tf.train.Example(features=tf.train.Features(feature=feature))
 
 
-record_file = 'images.tfrecords'
-with tf.io.TFRecordWriter(record_file) as writer:
-    tf_example = image_example(labrgb, gray, fixed_emb)
-    writer.write(tf_example.SerializeToString())
-
-
-# %% Read from TFRecords file
-
-raw_image_dataset = tf.data.TFRecordDataset('images.tfrecords')
-
-# Create a dictionary describing the features.
-image_feature_description = {
-    'image_raw': tf.io.FixedLenFeature([], tf.string),
-    'image_gray': tf.io.FixedLenFeature([], tf.string),
-    'caption_emb': tf.io.FixedLenFeature([], tf.string),
-}
-
 def _parse_image_function(example_proto):
   # Parse the input tf.train.Example proto using the dictionary above.
-  return tf.io.parse_single_example(example_proto, image_feature_description)
-
-parsed_image_dataset = raw_image_dataset.map(_parse_image_function)
-for image_features in parsed_image_dataset:
-    image_raw = image_features["image_raw"].numpy()
-    gray_raw = image_features["image_gray"].numpy()
-    caption = image_features["caption_emb"].numpy()
+  return tf.io.parse_single_example(example_proto, feature_desc)
 
 
-caption = np.frombuffer(caption, dtype=np.float32)
+def main():
+    # Read in example files
+    path_to_file = f"./tmp/caption15.txt"
+    path_to_img = f"./tmp/img15.jpeg"
+    file_exists = exists(path_to_file)
+    rgb_exists = exists(path_to_img)
+    if not file_exists or not rgb_exists:
+        print("Image doesnt exist")
+        raise AssertionError
+    with open(path_to_file) as f:
+        lines = f.readlines()
 
-image = np.frombuffer(image_raw, dtype=np.float64)
-image = np.resize(image, (256, 256, 3))
+    # Get Images
+    rgb = resize(io.imread(path_to_img), (256, 256))
+    gray = color.rgb2gray(rgb)
+    labrgb = color.rgb2lab(rgb)
 
-gray_i = np.frombuffer(gray_raw, dtype=np.float64)
-gray_i = np.resize(gray, (256, 256))
+    model = T5Model.from_pretrained("t5-small")
+    tok = T5Tokenizer.from_pretrained("t5-small")
 
-plt.imshow(image, cmap="gray")
-plt.figure()
-plt.imshow(gray_i, cmap="gray")
+    enc = tok(lines, max_length=1024, truncation=True, return_tensors="pt")
 
-assert (gray_i == gray).all()
-assert (image == labrgb).all()
-assert (caption == fixed_emb).all()
+    output = model.encoder(
+                input_ids=enc["input_ids"], 
+                attention_mask=enc["attention_mask"], 
+                return_dict=True)
+
+    # get the final hidden states
+    emb = output.last_hidden_state.detach().numpy().squeeze()
+    fixed_emb = emb.mean(axis=0)
+
+    # Write to tfrecords file 
+    record_file = 'images.tfrecords'
+    with tf.io.TFRecordWriter(record_file) as writer:
+        tf_example = image_example(labrgb, gray, fixed_emb)
+        writer.write(tf_example.SerializeToString())
+
+    # Read from TFRecords file
+    raw_image_dataset = tf.data.TFRecordDataset('images.tfrecords')
+
+    parsed_image_dataset = raw_image_dataset.map(_parse_image_function)
+    for image_features in parsed_image_dataset:
+        image_raw = image_features["image_raw"].numpy()
+        gray_raw = image_features["image_gray"].numpy()
+        caption = image_features["caption_emb"].numpy()
+
+
+    caption = np.frombuffer(caption, dtype=np.float32)
+
+    image = np.frombuffer(image_raw, dtype=np.float64)
+    image = np.resize(image, (256, 256, 3))
+
+    gray_i = np.frombuffer(gray_raw, dtype=np.float64)
+    gray_i = np.resize(gray, (256, 256))
+
+    plt.imshow(image, cmap="gray")
+    plt.figure()
+    plt.imshow(gray_i, cmap="gray")
+
+    assert (gray_i == gray).all()
+    assert (image == labrgb).all()
+    assert (caption == fixed_emb).all()
+
+
+
+if __name__ == "__main__":
+    main()
